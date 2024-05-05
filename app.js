cef7b4fb9b9032c3e16ac40abf02e7f67fca3ca8
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-05-05 17:29:24 -0400

    increase count + interval + callback

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ const updateCount = (el) => {
   const value = parseInt(el.dataset.value);
   const increment = Math.ceil(value / 1000);
   let initialValue = 0;
+
+  const increaseCount = setInterval(() => {
+    initialValue += increment;
+    if (initialValue > value) {
+      el.textContent = `${value}+`;
+      clearInterval(increaseCount);
+      return;
+    }
+    el.textContent = `${initialValue}+`;
+  }, 2);
 };
 
 numbersArray.forEach((number) => {
