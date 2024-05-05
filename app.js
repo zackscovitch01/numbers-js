48fe7e0632555a908d1fd4b3ec04c0c8320084cc
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-05-05 17:19:28 -0400

    initial updateCount() setup

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('numbers project');
+console.log("numbers project");
+
+const numbers = document.querySelectorAll(".number");
+const numbersArray = [...numbers];
+
+const updateCount = (el) => {
+  const value = parseInt(el.dataset.value);
+  const increment = Math.ceil(value / 1000);
+  let initialValue = 0;
+};
+
+numbersArray.forEach((number) => {
+  updateCount(number);
+});
